e04e5342b0a512374d2a06a5f38e4cb695023eff
Mukhesh113 <mukheshyanamasetty2@gmail.com>
2026-09-13 13:28:24 +0530

    Fix indentation and update example output

--- count occuerences.py
+++ count occuerences.py
@@ -6,6 +6,6 @@ for c in s:
         count += 1
 print("Occurrences:", count)
 #output:
-Enter a string: dharani
-Enter character to count: r
-Occurrences: 1
+Enter a string: mukhesh
+Enter character to count: h
+Occurrences: 2
