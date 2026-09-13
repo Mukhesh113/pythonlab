38073084a581cc92e9cf238a1cb612226d6140a0
Mukhesh113 <mukheshyanamasetty2@gmail.com>
2026-09-13 13:30:07 +0530

    Modify output example in second character script
    Updated output example in the script to reflect new input.

--- strings/second character.py
+++ strings/second character.py
@@ -1,5 +1,5 @@
 s = input("Enter a string: ")
 print("Every second character:", s[::2])
 #output:
-Enter a string: devi
-Every second character: dv
+Enter a string:mukhesh
+Every second character: mkeh
